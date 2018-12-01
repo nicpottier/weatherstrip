8b6afbf84b4a4be683cd37e75f5d804db1583e21
Nic Pottier <nicpottier@gmail.com>
2018-12-01 16:30:50 -0500

    first shot at lambda

--- weatherstrip.go
+++ weatherstrip.go
@@ -17,6 +17,9 @@ import (
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
 const (
@@ -235,7 +238,19 @@ func loadURLData(url string) ([]byte, error) {
 	return ioutil.ReadAll(resp.Body)
 }
 
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "Hello AWS Lambda and Netlify",
+	}, nil
+}
+
 func main() {
+	// Make the handler available for Remote Procedure Call by AWS Lambda
+	lambda.Start(handler)
+}
+
+func build() {
 	merged := make(map[time.Time]*HourForecast)
 
 	// read our telemetry file
